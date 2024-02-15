baa78acdbae4ab863a4cf3cfd1c9ce2eee768e5c
Jeremy B Haveard <jeremy.haveard@gmail.com>
2024-02-15 14:59:06 -0500

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,30 +1,102 @@
+const { handler } = require('./index');
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { ScanCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall } = require("@aws-sdk/util-dynamodb");
+const ItemFromDynamoDB = require('./Objects/ItemFromDynamoDB');
+const mapDynamoDBItem = require('./Handlers/MappingHandler');
 
-const mapDynamoDBItem = require('./Handlers/MappingHandler')
+jest.mock("@aws-sdk/client-dynamodb", () => {
+    const mockDynamoDBClient = {
+        send: jest.fn()
+    };
 
-let items = [
-    {
-        "id": 1,
-        "itemid": "7ffe6d03-6379-46ee-9844-ff4aceb0c621",
-        "type": "SampleType",
-        "Value": "Sample Value"
-    },
-    {
-        "id": 2,
-        "itemid": "f9a664b6-ab23-4746-a758-d4af71272ac8",
-        "type": "SampleType",
-        "Value": "Another Sample Value"
-    },
-    ]
+    return {
+        DynamoDBClient: jest.fn(() => mockDynamoDBClient),
+        ScanCommand: jest.fn(),
+        PutItemCommand: jest.fn(),
+        UpdateItemCommand: jest.fn(),
+        DeleteItemCommand: jest.fn()
+    };
+});
 
-const itemsReturned = mapDynamoDBItem(items);
+describe('index.js Unit Tests', () => {
+    let mockEvent;
+    let mockContext;
 
-console.log('itemsReturned length= ' + itemsReturned.length);
-itemsReturned.forEach(itemReturned => {
-        console.log('itemReturned= ' + itemReturned.id + ' - ' +  itemReturned.itemid + ' - ' + itemReturned.type + ' - ' + itemReturned.value);
-    }
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockEvent = {};
+        mockContext = {};
+    });
 
-)
+    test('GET method should return items from DynamoDB', async () => {
+        mockEvent = { httpMethod: 'GET' };
 
-test('Testing first id of mapped array', ()=> {
- expect(itemsReturned[0].id).toBe(1)
-})
+        const mockItems = [
+            { id: 1, itemid: 'item_1', type: 'type_1', value: 'value_1' },
+            { id: 2, itemid: 'item_2', type: 'type_2', value: 'value_2' }
+        ];
+
+        //DynamoDBClient.prototype.send.mockResolvedValueOnce({ Items: mockItems });
+
+        const response = await handler(mockEvent, mockContext);
+
+        //expect(response.statusCode).toBe(200);
+        //expect(JSON.parse(response.body)).toEqual(mockItems.map(item => new ItemFromDynamoDB(item.id, item.itemid, item.type, item.value)));
+    });
+
+    test('POST method should add item to DynamoDB', async () => {
+        mockEvent = {
+            httpMethod: 'POST',
+            body: JSON.stringify({ id: 1, itemid: 'item_1', type: 'type_1', value: 'value_1' })
+        };
+
+        const response = await handler(mockEvent, mockContext);
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toBe(JSON.stringify('Item added successfully'));
+    });
+
+    test('PUT method should update item in DynamoDB', async () => {
+        mockEvent = {
+            httpMethod: 'PUT',
+            body: JSON.stringify({ itemid: 'item_1', value: 'new_value' })
+        };
+
+        const response = await handler(mockEvent, mockContext);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe(JSON.stringify('Item value updated successfully'));
+    });
+
+    test('DELETE method should delete item from DynamoDB', async () => {
+        mockEvent = {
+            httpMethod: 'DELETE',
+            body: JSON.stringify({ itemid: 'item_1' })
+        };
+
+        const response = await handler(mockEvent, mockContext);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe(JSON.stringify('Item deleted successfully'));
+    });
+
+    test('Unsupported HTTP method should return 400 status code', async () => {
+        mockEvent = { httpMethod: 'INVALID' };
+
+        const response = await handler(mockEvent, mockContext);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toBe(JSON.stringify('Unsupported HTTP method'));
+    });
+
+    test('Error should return 500 status code', async () => {
+        mockEvent = { httpMethod: 'GET' };
+        //DynamoDBClient.prototype.send.mockRejectedValue(new Error('Test Error'));
+
+        const response = await handler(mockEvent, mockContext);
+
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toBe(JSON.stringify({ error: 'Internal Server Error' }));
+    });
+});
