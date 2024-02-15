9f898560341643a9701c3b5540b94058555bcde7
Jeremy B Haveard <jeremy.haveard@gmail.com>
2024-02-15 10:31:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,101 @@
-
-const mapDynamoDBItem = require('./Handlers/MappingHandler')
-const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, ScanCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand } = require("@aws-sdk/client-dynamodb");
+const { marshall } = require("@aws-sdk/util-dynamodb");
 const dynamoDbClient = new DynamoDBClient({ region: "us-east-1" });
+const mapDynamoDBItem = require('./Handlers/MappingHandler')
 
 exports.handler = async (event, context) => {
-  try {
-        // Parameters for the DynamoDB scan
-        const params = {
-          TableName: 'Item',
-          FilterExpression: 'attribute_exists(itemid)',
-        };
+    try {
+        const { httpMethod, body } = event;
 
-        // Create a command for the scan
-        const scanCommand = new ScanCommand(params);
+        console.log('Retrieved method:', httpMethod);
+        console.log('Retrieved body:', body);
 
-        // Execute the scan
-        const result = await dynamoDbClient.send(scanCommand);
+        if (httpMethod === 'GET') {
+            // Parameters for the DynamoDB scan
+            const params = {
+                TableName: 'Item',
+                FilterExpression: 'attribute_exists(itemid)',
+            };
 
-        // Process the result (you can customize this part based on your requirements)
-          console.log('Retrieved items:', result.Items);
+            // Create a command for the scan
+            const scanCommand = new ScanCommand(params);
 
-        const itemsFromDB = mapDynamoDBItem(result.Items)
+            // Execute the scan
+            const result = await dynamoDbClient.send(scanCommand);
 
-          console.log('Retrieved items:', itemsFromDB);
+            // Process the result (you can customize this part based on your requirements)
+            console.log('Retrieved items:', result.Items);
 
+            //const itemsFromDB = result.Items.map(item => unmarshall(item));
+            const itemsFromDB = mapDynamoDBItem(result.Items)
 
+            return {
+                statusCode: 200,
+                body: JSON.stringify(itemsFromDB)
+            };
+        } else if (httpMethod === 'POST') {
+            // Add functionality
+            const itemToAdd = JSON.parse(body);
+
+            const putParams = {
+                TableName: 'Item',
+                Item: marshall(itemToAdd),
+            };
+
+            const putCommand = new PutItemCommand(putParams);
+            await dynamoDbClient.send(putCommand);
+
+            return {
+                statusCode: 201,
+                body: JSON.stringify('Item added successfully')
+            };
+        } else if (httpMethod === 'PUT') {
+            // Update functionality
+            const itemToUpdate = JSON.parse(body);
+
+            const updateParams = {
+                TableName: 'Item',
+                Key: marshall({ itemid: itemToUpdate.itemid }), // Assuming itemid is the primary key
+                UpdateExpression: 'SET #attrName = :attrValue',
+                ExpressionAttributeNames: { '#attrName': 'value' }, // Change attribute_to_update to the actual attribute name you want to update
+                ExpressionAttributeValues: { ':attrValue': { S: itemToUpdate.value } }, // Change newValue to the new value you want to set
+                ReturnValues: 'UPDATED_NEW',
+            };
+
+            const updateCommand = new UpdateItemCommand(updateParams);
+            await dynamoDbClient.send(updateCommand);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify('Item value updated successfully')
+            };
+        } else if (httpMethod === 'DELETE') {
+            // Delete functionality
+            const itemToDelete = JSON.parse(body);
+
+            const deleteParams = {
+                TableName: 'Item',
+                Key: marshall({ itemid: itemToDelete.itemid }), // Assuming itemid is the primary key
+            };
+
+            const deleteCommand = new DeleteItemCommand(deleteParams);
+            await dynamoDbClient.send(deleteCommand);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify('Item deleted successfully')
+            };
+        } else {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Unsupported HTTP method')
+            };
+        }
+    } catch (error) {
+        console.error('Error:', error);
         return {
-          statusCode: 200,
-          body: itemsFromDB
-        };
-  } catch (error) {
-            console.error('Error retrieving items:', error);
-          return {
             statusCode: 500,
-          body: JSON.stringify({ error: 'Internal Server Error' })
+            body: JSON.stringify({ error: 'Internal Server Error' })
         };
-  }
+    }
 };
